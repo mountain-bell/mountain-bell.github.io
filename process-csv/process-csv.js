44003f79e4237aa33e9961f3bb0ba730cdd16fb9
mountain-bell <167797097+mountain-bell@users.noreply.github.com>
2024-05-23 08:21:57 +0900

    Add files via upload

--- process-csv/process-csv.js
+++ process-csv/process-csv.js
@@ -72,7 +72,7 @@
 			$('.error_msg').remove();
 			// ファイル選択用のinput要素からファイルを取得
 			const file = $('#upfile')[0].files[0];
-			if (!file) {
+			if(!file) {
 				let error_msg = 'ファイルが選択されていません！';
 				// ログに出力
 				console.log(error_msg);
@@ -96,25 +96,28 @@
 				rows = rows.map(row => row.trim());
 				// 行を回す
 				rows.forEach((row, rowIndex) => {
-					// 行をカンマで分割してセルに格納
-					const cells = row.split(',');
-					// 加工用変数をコピー
-					let input_process_copy = input_process;
-					// カラムを回す
-					cells.forEach((cell, columnIndex) => {
-						// カラム用変数を設定
-						let cell_no = 'cell_' + (columnIndex + 1);
-						// カラム置換用変数を設定
-						let cell_no_mark = `\{${cell_no}\}`;
-						// カラムの値を変数に設定
-						window[cell_no] = cell;
-						// ログに出力
-						console.log(cell_no + ' in row ' + (rowIndex + 1) + ':', window[cell_no]);
-						// 置換処理
-						input_process_copy = input_process_copy.split(cell_no_mark).join(window[cell_no]);
-					});
-					// 各行アウトプット連結
-					output_process = output_process + input_process_copy;
+					// 改行だけの行で置換処理を止める
+					if(row) {
+						// 行をカンマで分割してセルに格納
+						const cells = row.split(',');
+						// 加工用変数をコピー
+						let input_process_copy = input_process;
+						// カラムを回す
+						cells.forEach((cell, columnIndex) => {
+							// カラム用変数を設定
+							let cell_no = 'cell_' + (columnIndex + 1);
+							// カラム置換用変数を設定
+							let cell_no_mark = `\{${cell_no}\}`;
+							// カラムの値を変数に設定
+							window[cell_no] = cell;
+							// ログに出力
+							console.log(cell_no + ' in row ' + (rowIndex + 1) + ':', window[cell_no]);
+							// 置換処理
+							input_process_copy = input_process_copy.split(cell_no_mark).join(window[cell_no]);
+						});
+						// 各行アウトプット連結
+						output_process = output_process + input_process_copy;
+					}
 				});
 				// CSV加工アウトプット
 				$('#output_process').val(output_process);
